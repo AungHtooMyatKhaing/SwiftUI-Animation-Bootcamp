5ffa94ba9abd277580384c7db65c67aa1eeb7cd8
Aung Htoo Myat Khaing <aunghtoomyatkhaing@gmail.com>
2025-04-27 18:41:37 +0700

    Initial Commit

---
+++ SwiftUIAnimationBootcamp/SwiftUIAnimationBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationBootcampApp.swift
+//  SwiftUIAnimationBootcamp
+//
+//  Created by Aung Htoo Myat Khaing on 27/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
